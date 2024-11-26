ec11d32a3a2a8eefcaf87f9eb5dc799fe93d1f1b
matthewosgood <Mosgood777@gmail.com>
2024-11-26 18:48:08 -0500

    implemented board

--- Board.py
+++ Board.py
@@ -9,6 +9,16 @@ class Board:
         self.height = height
         self.board = np.zeros((height, width), dtype=np.int8)
         self.moves = 0
+        # stores the height of the top piece in each column
+        self.heights = np.zeros(width, dtype=np.int8)
+    
+    def copy(self, board):
+        b = Board()
+        b.width, b.height = self.width, self.height
+        b.board = self.board.copy()
+        b.moves = self.moves
+        b.height = self.heights.copy()
+        return b
 
     def __str__(self):
         s = ""
@@ -19,6 +29,10 @@ class Board:
         s += "-" * (self.width * 2 - 1) + "\n"
         for col in range(self.width):
             s += str(col) + " "
+        s += "\n"
+        for h in self.heights:
+            s += str(h) + " "
+        s += "h"
         return s
 
     def current_player(self):
@@ -26,19 +40,22 @@ class Board:
         return the current player
         """
         return self.moves % 2 + 1
+    
+    def can_play(self, col) -> bool:
+        """
+        return True if the column col is not full 
+        """
+        return self.heights[col] < self.height
 
-    def drop_piece(self, col) -> bool:
+    def drop_piece(self, col):
         """
         drop a piece in column col. The piece will be placed in the lowest available row in that column
+        assumes that the column is not full
         :param col: the column to place the piece ranges from 0 to width - 1
-        :return: True if the piece was successfully placed, False otherwise
         """
-        for row in range(self.height):
-            if self.board[row][col] == 0:
-                self.board[row][col] = self.current_player()
-                self.moves += 1
-                return True # for bug testing
-        return False
+        self.board[self.heights[col]][col] = self.current_player()
+        self.heights[col] += 1
+        self.moves += 1
 
     def is_full(self) -> bool:
         """
@@ -51,11 +68,41 @@ class Board:
         """
         return true if the current player playing in column col will result in a terminal state
         """
+        row = self.heights[col]
         # check vertical
-        for row in range(self.height - 3):
-            if self.board[row][col] == self.current_player and self.board[row + 1][col] == self.current_player and self.board[row + 2][col] == self.current_player and self.board[row + 3][col] == self.current_player:
+        if (row >= 3 and 
+            self.board[row - 1][col] == self.current_player() and
+            self.board[row - 2][col] == self.current_player() and
+            self.board[row - 3][col] == self.current_player()):
+            return True
+        # check horizontal
+        # for c in range(max(0, col - 3), min(self.width - 3, col)):
+        #     if (self.board[row][c] == self.current_player and
+        #             self.board[row][c + 1] == self.current_player and
+        #             self.board[row][c + 2] == self.current_player and
+        #             self.board[row][c + 3] == self.current_player):
+        #         return True
+        for dy in [-1, 0, 1]: # check negative slope diagonal, horizontal, positive slope diagonal
+            nb = 0
+            for dx in [-1, 1]: # check left and right
+                x = col + dx
+                y = row + dx * dy
+                while 0 <= x < self.width and 0 <= y < self.height and self.board[y][x] == self.current_player():
+                    nb += 1
+                    x += dx
+                    y += dx * dy
+            if nb >= 3:
                 return True
-
+        return False
+    
+    def play(self, moves):
+        for move in moves:
+            if self.can_play(int(move)):
+                self.drop_piece(int(move))
+            else:
+                print(f"Player {self.current_player()} failed to play in column {move}")
+                print(self)
+                return
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -7,14 +7,84 @@ class TestBoard(unittest.TestCase):
 
     def test_drop_piece(self):
         c4 = Board()
-        self.assertTrue(c4.drop_piece(0))
-        self.assertTrue(c4.drop_piece(0))
-        self.assertTrue(c4.drop_piece(0))
-        self.assertTrue(c4.drop_piece(0))
-        self.assertTrue(c4.drop_piece(0))
-        self.assertTrue(c4.drop_piece(0))
-        self.assertFalse(c4.drop_piece(0))
-        self.assertFalse(c4.drop_piece(0))
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[1][0])
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[2][0])
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[1][0])
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[2][0])
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[1][0])
+        c4.drop_piece(0)
+        self.assertEqual(c4.board[2][0])
         expected_board = np.zeros((6, 7), dtype=np.int8)
         expected_board[:, 0] = [1, 2, 1, 2, 1, 2]
         np.testing.assert_array_equal(c4.board, expected_board)
+        
+    def test_current_player(self):
+        c4 = Board()
+        self.assertEqual(c4.current_player(), Board.MAX)
+        c4.drop_piece(0)
+        self.assertEqual(c4.current_player(), Board.MIN)
+        c4.drop_piece(0)
+        self.assertEqual(c4.current_player(), Board.MAX)
+        
+    def test_can_play(self):
+        c4 = Board()
+        self.assertTrue(c4.can_play(0))
+        self.assertTrue(c4.can_play(c4.width - 1))
+        for i in range(c4.height):
+            self.assertTrue(c4.can_play(0))
+            c4.drop_piece(0)
+        self.assertFalse(c4.can_play(0))
+    
+    def test_is_full(self):
+        c4 = Board()
+        self.assertFalse(c4.is_full())
+        for i in range(c4.width):
+            for j in range(c4.height):
+                if c4.can_play(i):
+                    c4.drop_piece(i)
+        self.assertTrue(c4.is_full())
+        
+    def test_is_winning_move_vertical(self):
+        c4 = Board()
+        self.assertFalse(c4.is_winning_move(0))
+        c4.play("010101")
+        self.assertTrue(c4.is_winning_move(0))
+        self.assertFalse(c4.is_winning_move(1))
+
+    def test_is_winning_move_horizontal(self):
+        c4 = Board()
+        c4.play("001122")
+        self.assertTrue(c4.is_winning_move(3))
+        self.assertFalse(c4.is_winning_move(4))
+    
+    def test_is_winning_move_diagonal_positive(self):
+        c4 = Board()
+        """
+        0 0 0 0 0 0 0
+        0 0 0 0 0 0 0
+        0 0 0 0 0 0 0
+        0 0 1 2 0 0 0
+        0 1 1 2 0 0 0
+        1 2 2 2 1 0 0
+        """
+        c4.play("0112232343")
+        self.assertTrue(c4.is_winning_move(3))
+        
+    def test_is_winning_move_diagonal_negative(self):
+        """
+        0 0 0 1 0 0 0
+        0 0 0 1 1 0 0
+        0 0 0 2 1 1 0
+        0 0 0 1 2 2 1
+        0 0 0 2 1 1 2
+        0 0 2 2 2 2 1
+        """
+        c4 = Board()
+        c4.play("666555544443433330")
+        self.assertTrue(c4.is_winning_move(3))
+        
